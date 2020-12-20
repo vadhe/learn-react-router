7b6ef67a58dd729f3b3815a9af5aefcebe834252
vadhe <aldyvadhe@gmail.com>
2020-12-20 15:09:44 +0800

    add navbar component

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom"
 
 const Nav = () => {
     return (
-      <>
+      <div style={{ backgroundColor:"Red", width:"100%" }}>
         <Link to="/">Home</Link>
         <Link to="/about" style={{ marginLeft: "10px" }}>About</Link>
         <Link to="/topics" style={{ marginLeft: '10px' }}>Topics</Link>
-        </>
+     </div>
     )
 }
 
